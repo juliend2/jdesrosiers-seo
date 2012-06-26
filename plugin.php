03d4686997c5ffe46621360b5a73fa380e4d641b
Julien Desrosiers <juliend2@gmail.com>
2012-06-25 20:33:44 -0400

    initial commit

---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+Plugin Name: JDesrosiers SEO
+Description: A WordPress plugin with simple SEO features
+Author: Julien Desrosiers
+Version: 1.0
+Author URI: http://www.juliendesrosiers.com
+*/
+
+$jdseo_post_types = array(
+  'post',
+  'page'
+);
+
+
